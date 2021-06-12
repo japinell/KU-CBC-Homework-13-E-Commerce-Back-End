ff3103906bba37a3b54a4f6d3ccebd0ec31499dc
JAPinell <japinell@yahoo.com>
2021-06-12 01:30:43 -0500

    Added code to handle Product deletion

--- develop/routes/api/product-routes.js
+++ develop/routes/api/product-routes.js
@@ -1,22 +1,40 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({ include: [Category, Tag] });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [Category, Tag],
+    });
+    if (!product) {
+      res.status(404).json({ message: "Product not found!" });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +66,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +107,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({ where: { id: req.params.id } });
+    if (!product) {
+      res.status(404).json({ message: "Product not found!" });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
